2f708deb7b7654098258c40250d9f31f232cedbe
Francisco Alonso <francsco.a.alonso.s@gmail.com>
2015-06-20 11:42:13 -0430

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -2,30 +2,31 @@
 ## functions do
 
 ## This functions make use of the <<- assigment operator to store a matrix a its inverse 
-## in the 
+## in an enviroment created by makeCacheMatrix()
 
-## Write a short comment describing this function
+## Defines an enviroment and returns a list of functions that are in that enviroment.
+## It also defines objects 'x' and 'inverse' to store the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  inverse  <- NULL  #sets the inverse to NULL as the frst time t hasn´t been calculated
+  inverse  <- NULL  # Sets the inverse to NULL as the first time it hasn´t been calculated
   
-  #Sets the matrix to be cached and the defalt value NULL to the inverse
+  # Sets the matrix to be cached and the defalt value NULL to the inverse
   setMatrix <- function(m) 
   {
     x <<- m
     inverse <<- NULL
   }
   
-  #Returns the matrix
+  # Returns the matrix
   getMatrix <- function() x
   
-  #Sets the inverse to be cached 
+  # Sets the inverse to be cached 
   setInverse <- function(i) inverse <<- i
   
-  #Returns the inverse of the matrx
+  # Returns the inverse of the matrix
   getInverse <- function() inverse
   
-  
+  # Returns the set of functions to use 
   list(setMatrix = setMatrix
        , getMatrix = getMatrix
        , setInverse = setInverse
@@ -33,17 +34,26 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Stores and returns the inverse matrix of 'x'.
 
-cacheSolve <- function(f=matrix(), ...) {
-        ## Return a matrix that is the inverse of 'x'
+## IF the inverse hasn't been calculated yet t calculates the inverse
+## and stores it in the objects defined by makeCacheMatrix().
+
+## Parameter 'f' contains the list of functions defined by makeCacheMatrix().
+cacheSolve <- function(f, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  
+  # If the inverse has already being calculated and returns it
   inverse <- f$getInverse()
   if(!is.null(inverse))
   {
     return(inverse)
   }
+  # If not it calculates the inverse and set it to the appropiate variable
   m <- f$getMatrix()
-  inverse <- solve(m)
-  f$setInverse(inverse)
+  # Calculates the inverse
+  inverse <- solve(m) 
+  f$setInverse(inverse) 
+  # Returns the inverse
   inverse
 }
