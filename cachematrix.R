d792490a4446e14f2fc79bffd4f9c45b5324ad0f
Francisco Alonso <francsco.a.alonso.s@gmail.com>
2015-06-21 15:30:38 -0430

    Last commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## This functions make use of the <<- assigment operator to store a matrix a its inverse 
 ## in an enviroment created by makeCacheMatrix()
 
